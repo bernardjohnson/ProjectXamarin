52991e20d76c1eb3ff69156fc497145206cdf299
Bernard Johnson <bernardjohnson@outlook.com.br>
2017-05-22 14:51:59 -0300

    Projeto

---
+++ ProjetoXamarin/ProjetoXamarin/ProjetoXamarin/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoXamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjetoXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
